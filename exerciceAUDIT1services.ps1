cafe20846725dc49ff7f8f3d9bf71848d694ddc2
unknown <Administrateur@LOCAL.DORTMUND.CUB.SIOPLC.FR>
2025-03-05 10:51:45 +0100

    .

---
+++ exerciceAUDIT1services.ps1
@@ -0,0 +1,29 @@
+﻿#=======================================================
+#NAME:exerciceAUDIT1services.ps1
+#AUTHOR: Garnier Théo
+#DATE:26/02/2025
+#
+#=======================================================
+
+$outputFile = "C:\Users\Administrateur.LOCAL\Desktop\Powershell\contextecubsituation10\resultats\auditServices.txt"
+$Date = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
+$contenu = "Audit des services - Lancé le $Date`n"
+$services = @("DHCP", "DNS", "ADWS")
+foreach ($service in $services) {
+    $serviceStatus = Get-Service -Name $service -ErrorAction SilentlyContinue
+    $status = if ($serviceStatus.Status -eq "Running") {
+     "En cours d'exécution" 
+     } else {
+      "Stoppé ou introuvable" 
+     }
+    $contenu += "Service $service : $status`n"
+
+    if ($status -eq "En cours d'exécution") {
+        Write-Host "Service $service : $status" -ForegroundColor Green
+    } else {
+        Write-Host "Service $service : $status" -ForegroundColor Red
+    }
+}
+
+Set-Content -Path $outputFile -Value $contenu
+Write-Host "L'audit des services a été enregistré dans $outputFile"
---
+++ exerciceAUDIT2dhcp.ps1
@@ -0,0 +1,23 @@
+﻿#=======================================================
+#NAME:exerciceAUDIT2dhcp.ps1
+#AUTHOR: Garnier Théo
+#DATE:26/02/2025
+#
+#=======================================================
+
+$outputFile = "C:\Users\Administrateur.LOCAL\Desktop\Powershell\contextecubsituation10\resultats\auditDHCP.txt"
+$Date = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
+$contenu = "Audit des plages DHCP - Lancé le $Date`n"
+$scopes = Get-DhcpServerv4Scope
+foreach ($scope in $scopes) {
+    $contenu += "Plage $($scope.Name) : $($scope.State)`n" 
+    
+    if ($scope.State -eq "Active") {
+        Write-Host "Plage $($scope.Name) :" $scope.State -ForegroundColor Green
+    } else {
+        Write-Host "Plage $($scope.Name) :" $scope.State -ForegroundColor Red
+    }
+}
+
+Set-Content -Path $outputFile -Value $contenu
+Write-Host "`nL'audit des plages DHCP a été enregistré dans $outputFile"
